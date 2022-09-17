73cf80a96447d4c52005b6e6948e5cb2998ac961
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-09-18 02:24:54 +0500

    Hiding add to cart if not login

--- Restaurant-App/src/components/Items.js
+++ Restaurant-App/src/components/Items.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios'
+import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
@@ -10,12 +11,15 @@ import Form from 'react-bootstrap/Form';
 function Items({items , categorys}) {
     const [show, setShow] = useState(false);
     const [quantity, setQuantity] = useState()
-
+    const getstatus = localStorage.getItem('status');
     
     const [category, setcategory] = useState(
         []
       )
       const [item, setItem] = useState([])
+      function location() {
+        window.location.href="/login"
+      }
       const handleClose = () => setShow(false);
       async function AddtoCart() {
         const cartDetail = {
@@ -68,28 +72,51 @@ function Items({items , categorys}) {
                         ):(
                         <></>)}
                       
-
-                      <Modal show={show} onHide={handleClose}>
+{getstatus === "true" ? (<>
+  <Modal show={show} onHide={handleClose}>
                      
-        <Modal.Header closeButton>
-          <Modal.Title>{items.Title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <h5> ${items.Price}</h5>
-            <p>{items.Title}</p>
-            <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Quantity</Form.Label>
-        <Form.Control type="number" value={quantity} onChange={(e) => { setQuantity(e.target.value) }} required />
-      </Form.Group>
-      </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" size="lg" style={{width:"100%"}} onClick={AddtoCart}>
-            Add to Cart
-          </Button>
-        </Modal.Footer>
-      </Modal>
+                     <Modal.Header closeButton>
+                       <Modal.Title>{items.Title}</Modal.Title>
+                     </Modal.Header>
+                     <Modal.Body>
+                         <h5> ${items.Price}</h5>
+                         <p>{items.Title}</p>
+                         <Form>
+                   <Form.Group className="mb-3" controlId="formBasicEmail">
+                     <Form.Label>Quantity</Form.Label>
+                     <Form.Control type="number" value={quantity} onChange={(e) => { setQuantity(e.target.value) }} required />
+                   </Form.Group>
+                   </Form>
+                     </Modal.Body>
+                     <Modal.Footer>
+                         <Button variant="primary" size="lg" style={{width:"100%"}} onClick={AddtoCart}>
+                         Add to Cart
+                       </Button>
+
+             
+                     </Modal.Footer>
+                   </Modal>
+</>):(<>
+  <Modal show={show} onHide={handleClose}>
+                     
+
+                     <Modal.Body>
+                     <Modal.Header closeButton>
+                       <Modal.Title>Alert</Modal.Title>
+                     </Modal.Header>
+                     <h3>Dear user, you must have in login first to add to cart. Thank you!</h3> 
+
+                     </Modal.Body>
+                     <Modal.Footer>
+
+                     <Button variant="primary" size="lg" style={{width:"100%"}} onClick={location}>
+                         Login
+                       </Button>
+
+                     </Modal.Footer>
+                   </Modal>
+</>)}
+
        {/* <ToastContainer /> */}
     </>
 )
