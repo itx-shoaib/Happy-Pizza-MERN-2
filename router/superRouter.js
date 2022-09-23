4c1b87f26798a0d7639d3e638d76cc871809cb16
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-09-23 21:58:54 +0500

    Making getliveresturants

--- router/superRouter.js
+++ router/superRouter.js
@@ -2,6 +2,21 @@ const express = require("express");
 const router = express.Router();
 const dbconfig = require('../db');
 
-
+// Router 1: http://localhost:5000/api/superadmin/getliveresturants
+// Status:
+router.get('/getliveresturants',(req,res)=>{
+    let qr = `Select * from resturant where status = "true"`
+    dbconfig.query(qr,(err,result)=>{
+        if (!err) {
+            res.json({
+                data:result
+            })
+        } else {
+            res.status(404).json({
+                error:err
+            })
+        }
+    })
+})
 
 module.exports = router
