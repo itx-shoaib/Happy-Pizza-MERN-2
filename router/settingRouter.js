5032af0ada659c92eee56cfc7eb2208da4a13fb5
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-09-23 21:51:41 +0500

    Updating addresturant APi and removing it from superRouter

--- router/settingRouter.js
+++ router/settingRouter.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const dbconfig = require('../db');
 
-
-// Router 1 : Registering the resturant information https://apinodejs.creativeparkingsolutions.com/api/setting/resturantmanagement
+// Router 1 api is for super admin and admin for adding the resturnat.
+// Router 1 : Registering the resturant information https://apinodejs.creativeparkingsolutions.com/api/setting/addresturantmanagement
 // Status
-router.post('/resturantmanagement',(req,res)=>{
+router.post('/addresturant',(req,res)=>{
     let name = req.body.name;
     let description = req.body.description;
     let address = req.body.address;
@@ -15,33 +15,64 @@ router.post('/resturantmanagement',(req,res)=>{
     let average_order = req.body.average_order;
     let time = req.body.time;
     let owner_name = req.body.owner_name;
-    let email = req.body.email;
+    let owner_email = req.body.owner_email;
+    let owner_address = req.body.owner_address;
     let owner_phone = req.body.owner_phone;
-
-    // Writing the first query for the already exist in db.
-    let qr = `SELECT * FROM resturant WHERE name = ${name} and address = ${address};`
+    let domain = req.body.domain;
+    let primary_color = req.body.primary_color;
+    let secondary_color = req.body.secondary_color;
+    let app_name = req.body.app_name;
+    let delivery_min = req.body.delivery_min
+    let location_search = req.body.location_search;
+    let stripe_connect = req.body.stripe_connect
+    let enable_stripe = req.body.enable_stripe;
+    let stripe_key = req.body.stripe_key;
+    let stripe_secret = req.body.stripe_secret;
+    let map_api = req.body.map_api;
+    let analytics = req.body.analytics;
+    let client_id = req.body.client_id;
+    let client_secret = req.body.client_secret;
+    let redirect = req.body.redirect;
+    let fclient_id = req.body.fclient_id;
+    let fclient_secret = req.body.fclient_secret;
+    let fclient_redirect = req.body.fclient_redirect;
+    let app_id = req.body.app_id;
+    let rapi_key = req.body.rapi_key;
+    let sms = req.body.sms;
+    let optomany_enabled = req.body.optomany_enabled;
+    let oclient_id = req.body.oclient_id;
+    let oclient_secret = req.body.oclient_secret;
+    let oterminal_id = req.body.oterminal_id;
+    let otest_mode = req.body.otest_mode;
+    
+    let qr = `Select * from resturant where owner_email = "${owner_email}" or name = "${name}"`
     dbconfig.query(qr,(err,result)=>{
-        if (err) {
-            // Writing second query for registering the resturant
-            let qr = `insert into resturant(name,description,address,phone,charges,minimum_order,average_order,time,owner_name,email,owner_phone)
-            values('${name}','${description}','${address}','${phone}','${charges}',${minimum_order},${average_order},${time},'${owner_name}','${email}','${owner_phone}')`
-            dbconfig.query(qr,(err,result)=>{
-                if(!err){
+        if (!err) {
+            if (result.length <=0) {
+                let qr = `INSERT INTO resturant(owner_name, owner_email,owner_address,owner_phone, domain,primary_color,secondary_color,app_name,delivery_min,location_search,stripe_connect, enable_stripe,stripe_key,stripe_secret,map_api,analytics,client_id,client_secret,redirect, fclient_id,fclient_secret,fclient_redirect,app_id,rapi_key,sms,optomany_enabled,oclient_id, oclient_secret,oterminal_id,otest_mode,name,description,address,phone,charges,minimum_order,average_order,time) VALUES ('${owner_name}', '${owner_email}', '${owner_address}', '${owner_phone}', '${domain}', '${primary_color}', '${secondary_color}', '${app_name}', '${delivery_min}', '${location_search}', '${stripe_connect}', '${enable_stripe}', '${stripe_key}', '${stripe_secret}', '${map_api}', '${analytics}', '${client_id}', '${client_secret}', '${redirect}', '${fclient_id}', '${fclient_secret}', '${fclient_redirect}', '${app_id}', '${rapi_key}', '${sms}', '${optomany_enabled}', '${oclient_id}', '${oclient_secret}', '${oterminal_id}', '${otest_mode}','${name}','${description}','${address}','${phone}','${charges}',${minimum_order},${average_order},${time});`
+                if (result.length > 0) {
                     res.json({
-                        message:"Data has been inserted"
+                        message:"data has been inserted"
                     })
-                }
-                else{
-                    console.log(err,"err")
-                }
-            })
+                } else {
+                    res.status(404).json({
+                        error:err
+                    })
+                }   
+            } else {
+                res.json({
+                    message:"Email already existed"
+                })
+            }
         } else {
-            res.status(404).json({
-                message:"Name or address already exist"
-            
+            res.json({
+                error:err
             })
         }
     })
+
+
+
 })
 
 
--- router/superRouter.js
+++ router/superRouter.js
@@ -2,69 +2,6 @@ const express = require("express");
 const router = express.Router();
 const dbconfig = require('../db');
 
-// Router 1: http://localhost:5000/api/superadmin/addresturant
-// Status:
-router.post('/addresturant',(req,res)=>{
-    let name = req.body.name;
-    let owner_name = req.body.owner_name;
-    let owner_email = req.body.owner_email;
-    let owner_address = req.body.owner_address;
-    let owner_phone = req.body.owner_phone;
-    let domain = req.body.domain;
-    let primary_color = req.body.primary_color;
-    let secondary_color = req.body.secondary_color;
-    let app_name = req.body.app_name;
-    let delivery_min = req.body.delivery_min
-    let location_search = req.body.location_search;
-    let stripe_connect = req.body.stripe_connect
-    let enable_stripe = req.body.enable_stripe;
-    let stripe_key = req.body.stripe_key;
-    let stripe_secret = req.body.stripe_secret;
-    let map_api = req.body.map_api;
-    let analytics = req.body.analytics;
-    let client_id = req.body.client_id;
-    let client_secret = req.body.client_secret;
-    let redirect = req.body.redirect;
-    let fclient_id = req.body.fclient_id;
-    let fclient_secret = req.body.fclient_secret;
-    let fclient_redirect = req.body.fclient_redirect;
-    let app_id = req.body.app_id;
-    let rapi_key = req.body.rapi_key;
-    let sms = req.body.sms;
-    let optomany_enabled = req.body.optomany_enabled;
-    let oclient_id = req.body.oclient_id;
-    let oclient_secret = req.body.oclient_secret;
-    let oterminal_id = req.body.oterminal_id;
-    let otest_mode = req.body.otest_mode;
-    
-    let qr = `Select * from superresturant where owner_email = "${owner_email}" or name = "${name}"`
-    dbconfig.query(qr,(err,result)=>{
-        if (!err) {
-            if (result.length <=0) {
-                let qr = `INSERT INTO superresturant(name, owner_name, owner_email,owner_address,owner_phone, domain,primary_color,secondary_color,app_name,delivery_min,location_search,stripe_connect, enable_stripe,stripe_key,stripe_secret,map_api,analytics,client_id,client_secret,redirect, fclient_id,fclient_secret,fclient_redirect,app_id,rapi_key,sms,optomany_enabled,oclient_id, oclient_secret,oterminal_id,otest_mode) VALUES ('${name}', '${owner_name}', '${owner_email}', '${owner_address}', '${owner_phone}', '${domain}', '${primary_color}', '${secondary_color}', '${app_name}', '${delivery_min}', '${location_search}', '${stripe_connect}', '${enable_stripe}', '${stripe_key}', '${stripe_secret}', '${map_api}', '${analytics}', '${client_id}', '${client_secret}', '${redirect}', '${fclient_id}', '${fclient_secret}', '${fclient_redirect}', '${app_id}', '${rapi_key}', '${sms}', '${optomany_enabled}', '${oclient_id}', '${oclient_secret}', '${oterminal_id}', '${otest_mode}');`
-                if (!err) {
-                    res.json({
-                        message:"data has been inserted"
-                    })
-                } else {
-                    res.status(404).json({
-                        message:"somehting wrong"
-                    })
-                }   
-            } else {
-                res.json({
-                    message:"Email already existed"
-                })
-            }
-        } else {
-            res.json({
-                error:err
-            })
-        }
-    })
 
-
-
-})
 
 module.exports = router
