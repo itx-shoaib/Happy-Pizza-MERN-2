3c6a38078620a4ac71586f676cb311b4d98bca8b
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-10-01 23:11:42 +0500

    Image upload in progress

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -6,7 +6,7 @@ const multer = require("multer")
 // Image storage connfig
 var imgconfig = multer.diskStorage({
     destination:(req,file,callback)=>{
-        callback(null,"../uploads");
+        callback(null,"/uploads");
     },
     filename:(req,file,callback)=>{
         callback(null,`image-${Date.now()}.${file.originalname}`)
