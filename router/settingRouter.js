ca34e7baac440b04ed7f9863983e01770cabbe7a
itx-shoaib <shoaibjamil43@gmail.com>
2022-10-20 13:08:45 -0700

    Making the addgallery api

--- router/settingRouter.js
+++ router/settingRouter.js
@@ -490,4 +490,52 @@ router.post("/addtimings",(req,res)=>{
     })
 })
 
+// Router for Gallery / Path: http://localhost:5000/api/setting/addgallery
+router.post("/addgallery",upload.single("image"),(req,res)=>{
+    let id = req.body.id;
+    let link = req.body.link;
+    const filename = req.file.path;
+
+    let qr = `Select count(*) as 'total' from gallery where resturant_ID = ${id} `
+    dbconfig.query(qr,(err,result)=>{
+        if (!err) {
+            if (result[0]['total'] === 0) {
+                let qr = `Insert into gallery (resturant_ID,link,image)
+                values (${id},'${link}','${filename}') `
+                dbconfig.query(qr,(err,result)=>{
+                    if (!err) {
+                        res.status(200).json({
+                            message:"Data has been inserted"
+                        })
+                    } else {
+                        res.status(404).json({
+                            message:"Incomplete requirements"
+                        })
+                    }
+                })
+            } else {
+                let qr = `Update timing 
+                set link = '${link}',
+                image ='${filename}'
+                where resturant_ID = ${id} `
+                dbconfig.query(qr,(err,result)=>{
+                    if (!err) {
+                        res.status(200).json({
+                            message:"Data has been updated"
+                        })
+                    } else {
+                        res.status(404).json({
+                            message:"Invalid requirements"
+                        })
+                    }
+                })
+            }
+        } else {
+            res.status(500).json({
+                message:"Something went wrong with this link"
+            })
+        }
+    })
+})
+
 module.exports = router
