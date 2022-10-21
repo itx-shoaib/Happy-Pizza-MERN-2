a492b7f01f7b4b43bc9d63ee637de0522252dda0
itx-shoaib <shoaibjamil43@gmail.com>
2022-10-21 02:31:10 -0700

    updating orderreport api

--- router/superRouter.js
+++ router/superRouter.js
@@ -175,7 +175,7 @@ router.post('/updatepagestatus',(req,res)=>{
 // Router 8:  https://apinodejs.creativeparkingsolutions.com/api/superadmin/orderreport
 // Status: working
 router.get('/orderreport',(req,res)=>{
-    let qr = `SELECT *,customer.name as "cname" FROM cart INNER join customer on cart.customer_Id = customer.customer_Id 
+    let qr = `SELECT *,customer.name as "cname",cart.DateTime as "datetime" FROM cart INNER join customer on cart.customer_Id = customer.customer_Id 
     INNER join resturant on cart.resturant_ID = resturant.ID`;
 
     dbconfig.query(qr,(err,result)=>{
