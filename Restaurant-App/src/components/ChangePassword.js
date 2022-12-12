7a2bb8d10a99bfbd1662a0c6c2e5b8d2fc8dd59e
itx-shoaib <shoaibjamil43@gmail.com>
2022-12-12 16:32:59 +0500

    Integrating change password api

--- Restaurant-App/src/components/ChangePassword.js
+++ Restaurant-App/src/components/ChangePassword.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import Swal from 'sweetalert2'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ChangePassword() {
   const getstatus = localStorage.getItem("status");
+  const [password, setpassword] = useState("")
+  const [new_password, setnew_password] = useState("")
+  const [cpassword, setcpassword] = useState("")
+  async function register(e) {
+    if (new_password === cpassword) {
+      const details = {
+        customer_Id: JSON.parse(localStorage.getItem('currentuser'))[0].customer_Id,
+        email: JSON.parse(localStorage.getItem('currentuser'))[0].email,
+        password,
+        new_password
+      }
+      try {
+
+        // setloading(true)
+        const result = await axios.post("http://localhost:5000/api/admin/changepassword", details).data;
+        toast.success("Password has been changed")
+        // setloading(true)
+        setInterval(() => {
+          window.location.href = "/home"
+        }, 2000);
+
+
+
+
+        setnew_password('')
+        setpassword('')
+        setcpassword('')
+
+      } catch (error) {
+        console.log(error);
+        toast.warn("Something went wrong!")
+        e.preventDefault()
+        // setloading(true)
+      }
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Password and Confirm password are not matched!'
+      })
+      e.preventDefault()
+    }
+  }
   return (
     <>
+      <ToastContainer />
       <div className="hidescroll">
         <Navbar />
         <div className="container-fluid">
@@ -86,18 +134,24 @@ function ChangePassword() {
                     id="password"
                     className="form-control mb-4 px-4 py-3"
                     placeholder="Current Password"
+                    value={password}
+                    onChange={(e) => { setpassword(e.target.value) }}
                   />
                   <input
                     id="npassword"
                     className="form-control mb-4 px-4 py-3"
                     placeholder="New Password"
+                    value={new_password}
+                    onChange={(e) => { setnew_password(e.target.value) }}
                   />
                   <input
                     id="cpassword"
                     className="form-control mb-4 px-4 py-3"
                     placeholder="Confirm New Password"
+                    value={cpassword}
+                    onChange={(e) => { setcpassword(e.target.value) }}
                   />
-                  <button className="btn btn-primary usercartbtn">
+                  <button className="btn btn-primary usercartbtn" onClick={register}>
                     Change Password
                   </button>
                 </div>
